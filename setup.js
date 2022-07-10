24a222788093cb32ec0e7b7d386eeb2627c03d00
suraj-chadha <s.chadha234@gmail.com>
2022-07-10 21:51:34 +0530

    :memo: added sticky notes for note taking,todos,reminders,etc

--- setup.js
+++ setup.js
@@ -6,10 +6,13 @@ tool.strokeStyle = "black";
 tool.lineWidth = 5;
 let boardTop = canvasBoard.getBoundingClientRect().top + 5;
 let boardLeft = canvasBoard.getBoundingClientRect().left + 5;
+
 let recTool = document.querySelector('#rect');
 let lineTool = document.querySelector('#line');
 let pencilTool = document.querySelector('#pencil');
 let eraserTool = document.querySelector('#eraser');
+let sticky = document.querySelector('#sticky');
+
 let cTool = "pencilTool";
 let sizeBoxes = document.querySelectorAll('.size-box');
 let red = document.querySelector('.red');
@@ -23,3 +26,4 @@ let pencilSize = 5;
 let lineSize = 5;
 let eraserSize = 5;
 let rectSize = 5;
+
---
+++ sticky.js
@@ -0,0 +1,29 @@
+
+let body = document.querySelector('body');
+
+sticky.addEventListener("click", function () {
+    let sticky = document.createElement('div');
+    sticky.setAttribute('class', 'sticky');
+    sticky.innerHTML = `<div class="navbar">
+            <div class="minimize"></div>
+            <div class="close"></div>
+        </div>
+        <textarea name="" id="" cols="30" rows="10" class="textarea"></textarea>`;
+    body.appendChild(sticky);
+
+    let minimize = sticky.querySelector('.minimize');
+    let textarea = sticky.querySelector('.textarea');
+    let close = sticky.querySelector('.close');
+
+    minimize.addEventListener('click', () => {
+        if (textarea.style.display == "none") {
+            textarea.style.display = "block";
+        } else {
+            textarea.style.display = "none";
+        }
+
+    })
+    close.addEventListener('click', () => {
+        sticky.remove();
+    })
+})
